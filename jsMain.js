376935a9a38f9957127e9bed8f511225ab9ef59d
Yorvin Silva <yorvinsilvamexico@gmail.com>
2022-09-10 22:37:23 -0500

    segunda edicion

--- jsMain.js
+++ jsMain.js
@@ -1,7 +1,12 @@
-function openmodal(){
+const modal1 = document.getElementById("modal1");
+const fullimg = document.getElementById("fullimg");
+
+function openmodal(reference){
   modal1.style.display="flex";
+  fullimg.src = reference
 }
 
 function closemodal(){
     modal1.style.display="none";
+    
   }
